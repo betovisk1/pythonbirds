0977865fecd4945d5c895792c563760c4b5f27ad
Beto <betovisk1@hotmail.com>
2018-04-12 20:45:38 -0300

    Criação da classe Pessoa

---
+++ oo/pessoa.py
@@ -0,0 +1,12 @@
+from lib2to3 import main
+
+
+class Pessoa:
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
