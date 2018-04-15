8cba246a950519e96814b2fad54052b6a1fbbfcf
Beto <betovisk1@hotmail.com>
2018-04-15 10:36:33 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,11 +2,20 @@ from lib2to3 import main
 
 
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
-    print(p.cumprimentar())
+    print(p.cumprimentar)
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
+    print(p.idade)
+    print(p.id)
